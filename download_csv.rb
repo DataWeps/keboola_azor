e73112548b75475ab589292f2f163e1137efae97
Jan Mosat <jmosat@gmail.com>
2017-05-03 18:22:21 +0200

    [MOD] allow download of multiple files

--- download_csv.rb
+++ download_csv.rb
@@ -1,17 +1,18 @@
 require 'json'
 require 'typhoeus'
 
+API_FILE_LIST_URL = 'https://azor.weps.cz/api/files'.freeze
 API_URL = 'https://azor.weps.cz/api/files/#FILE_ID/download'.freeze
 MANIFEST = {
   incremental: true,
   primary_key: %w(internal_code shop saved_at)
 }.freeze
+ALLOWED_FILE_REGEXP = /autoexport$|portfolio$/
 
 
-def download_file
-  file = File.open(filename, 'wb')
-  url = API_URL.sub('#FILE_ID', CONFIG['file'])
-  password = CONFIG['#password'] ? CONFIG['#password'] : CONFIG['password']
+def download_file(token)
+  file = File.open(filename(token), 'wb')
+  url = API_URL.sub('#FILE_ID', token)
   request = Typhoeus::Request.new(
     url,
     userpwd: "#{CONFIG['username']}:#{password}"
@@ -31,7 +32,8 @@ end
 def fetch_files
   tries = 5
   begin
-    download_file
+    tokens = fetch_file_tokens
+    tokens.each { |token| download_file(token) }
   rescue => exception
     unless exception.to_s == 'Request failed'
       STDERR.puts "#{exception.class}: #{exception.message}"
@@ -46,14 +48,32 @@ def fetch_files
   end
 end
 
-def filename
-  "#{ENV['KBC_DATADIR']}out/tables/out.c-azor.#{CONFIG['file']}.csv"
+def fetch_file_tokens
+  response = Typhoeus::Request.new(
+    API_FILE_LIST_URL,
+    userpwd: "#{CONFIG['username']}:#{password}"
+  ).run
+  json = JSON.parse(response.body)
+  client = json['client']
+  tokens = json['files'].collect { |file| file['token'] }
+  tokens.keep_if do |token|
+    token.start_with?(client) && token =~ ALLOWED_FILE_REGEXP
+  end
+  tokens
+end
+
+def filename(token)
+  "#{ENV['KBC_DATADIR']}out/tables/out.c-azor.#{token}.csv"
 end
 
 def create_manifest
-  Dir["#{ENV['KBC_DATADIR']}out/tables/*.csv"].each do |table|
+  Dir["#{ENV['KBC_DATADIR']}out/tables/*autoexport.csv"].each do |table|
     File.open("#{table}.manifest", 'w') { |file| file << MANIFEST.to_json }
   end
+end
+
+def password
+  CONFIG['#password'] ? CONFIG['#password'] : CONFIG['password']
 end
 
 begin
